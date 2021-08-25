1abcd263d71209babcdf97d74a91bc96cd24a280
Felix <felixrunye@gmail.com>
2021-08-25 11:53:51 +0300

    updated nav and resposive header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  mode:'jit',
+  mode:'jit',//just in time compiler
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      animation: ['group-hover'],
+    },
   },
   plugins: [],
 }
