b425d42eb7391d69c45a1dcb9b631f9255eb7c6c
Felix <felixrunye@gmail.com>
2022-01-12 19:40:00 +0300

    result update

--- components/Result.js
+++ components/Result.js
@@ -1,14 +1,14 @@
 import Thumbnail from "./Thumbnail"
 import FlipMove from "react-flip-move";
 
-function Result( { results } ) {
+function Result({ results }) {
     if (!Array.isArray(results)) return 'results are not array'
     return (
         <FlipMove className='px-5 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
-            {results.map((result) =>(
+            {results.map((result) => (
                 <Thumbnail key={result.id} result={result} />
-                
-            ) ) }
+
+            ))}
         </FlipMove>
     )
 }
